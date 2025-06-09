62755bcdc101071808df5d0d413c87f66b273a80
Hailey <117295822+hmccurryWM@users.noreply.github.com>
2025-06-09 14:53:23 -0500

    initial commit
    very very very messy implementation to use with an HEP-1000-100 on an esp32. Generalized some, but not enough. Not current.

---
+++ Meanwell-PMBus-commands.h
@@ -0,0 +1,168 @@
+/**
+ * This file contains #defines for all of the commands supported by the RPB-1600
+ * It also includes data length and N values for decoding/encoding values
+ * See the PMBus 1.1 specifications "Linear data" section for more info
+ */
+
+// Command Codes
+#define CMD_CODE_OPERATION 0x01
+#define CMD_CODE_ON_OFF_CONFIG 0x02
+#define CMD_CODE_CAPABILITY 0x19
+#define CMD_CODE_VOUT_MODE 0x20
+#define CMD_CODE_VOUT_COMMAND 0x21
+#define CMD_CODE_VOUT_TRIM 0x22
+#define CMD_CODE_IOUT_OC_FAULT_LIMIT 0x46
+#define CMD_CODE_IOUT_OC_FAULT_RESPONSE 0x47
+#define CMD_CODE_STATUS_WORD 0x79
+#define CMD_CODE_STATUS_VOUT 0x7A
+#define CMD_CODE_STATUS_IOUT 0x7B
+#define CMD_CODE_STATUS_INPUT 0x7C
+#define CMD_CODE_STATUS_TEMPERATURE 0x7D
+#define CMD_CODE_STATUS_CML 0x7E
+#define CMD_CODE_STATUS_MFR_SPECIFIC 0x80
+//#define CMD_CODE_STATUS_FANS_1_2 0x81
+#define CMD_CODE_READ_VIN 0x88
+#define CMD_CODE_READ_VOUT 0x8B
+#define CMD_CODE_READ_IOUT 0x8C
+#define CMD_CODE_READ_TEMPERATURE_1 0x8D
+//#define CMD_CODE_READ_FAN_SPEED_1 0x90
+//#define CMD_CODE_READ_FAN_SPEED_2 0x91
+#define CMD_CODE_PMBUS_REVISION 0x98
+#define CMD_CODE_MFR_ID 0x99
+#define CMD_CODE_MFR_MODEL 0x9A
+#define CMD_CODE_MFR_REVISION 0x9B
+#define CMD_CODE_MFR_LOCATION 0x9C
+#define CMD_CODE_MFR_DATE 0x9D
+#define CMD_CODE_MFR_SERIAL 0x9E
+
+// Charing Curve config command codes
+#define CMD_CODE_CURVE_CC 0xB0
+#define CMD_CODE_CURVE_CV 0xB1
+#define CMD_CODE_CURVE_FV 0xB2
+#define CMD_CODE_CURVE_TC 0xB3
+#define CMD_CODE_CURVE_CONFIG 0xB4
+#define CMD_CODE_CURVE_CC_TIMEOUT 0xB5
+#define CMD_CODE_CURVE_CV_TIMEOUT 0xB6
+#define CMD_CODE_CURVE_FLOAT_TIMEOUT 0xB7
+#define CMD_CODE_CHG_STATUS 0xB8
+
+// System Stuff
+#define CMD_CODE_SYSTEM_CONFIG 0xBE
+#define CMD_CODE_SYSTEM_STATUS 0xBF
+
+// Command lengths in bytes
+#define CMD_LENGTH_OPERATION 1
+#define CMD_LENGTH_ON_OFF_CONFIG 1
+#define CMD_LENGTH_CAPABILITY 1
+#define CMD_LENGTH_VOUT_MODE 1
+#define CMD_LENGTH_VOUT_COMMAND 2
+#define CMD_LENGTH_VOUT_TRIM 2
+#define CMD_LENGTH_IOUT_OC_FAULT_LIMIT 2
+#define CMD_LENGTH_IOUT_OC_FAULT_RESPONSE 1
+#define CMD_LENGTH_STATUS_WORD 2
+#define CMD_LENGTH_STATUS_VOUT 1
+#define CMD_LENGTH_STATUS_IOUT 1
+#define CMD_LENGTH_STATUS_INPUT 1
+#define CMD_LENGTH_STATUS_TEMPERATURE 1
+#define CMD_LENGTH_STATUS_CML 1
+#define CMD_LENGTH_STATUS_MFR_SPECIFIC 1
+//#define CMD_LENGTH_STATUS_FANS_1_2 1
+#define CMD_LENGTH_READ_VIN 2
+#define CMD_LENGTH_READ_VOUT 2
+#define CMD_LENGTH_READ_IOUT 2
+#define CMD_LENGTH_READ_TEMPERATURE_1 2
+//#define CMD_LENGTH_READ_FAN_SPEED_1 2
+//#define CMD_LENGTH_READ_FAN_SPEED_2 2
+#define CMD_LENGTH_PMBUS_REVISION 1
+#define CMD_LENGTH_MFR_ID 12
+#define CMD_LENGTH_MFR_MODEL 12
+#define CMD_LENGTH_MFR_REVISION 6
+#define CMD_LENGTH_MFR_LOCATION 3
+#define CMD_LENGTH_MFR_DATE 6
+#define CMD_LENGTH_MFR_SERIAL 12
+#define CMD_LENGTH_SYSTEM_CONFIG 2
+#define CMD_LENGTH SYSTEM_STATUS 2
+
+// Charing Curve config command lengths in bytes
+#define CMD_LENGTH_CURVE_CC 2
+#define CMD_LENGTH_CURVE_CV 2
+#define CMD_LENGTH_CURVE_FV 2
+#define CMD_LENGTH_CURVE_TC 2
+#define CMD_LENGTH_CURVE_CONFIG 2
+#define CMD_LENGTH_CURVE_CC_TIMEOUT 2
+#define CMD_LENGTH_CURVE_CV_TIMEOUT 2
+#define CMD_LENGTH_CURVE_FLOAT_TIMEOUT 2
+#define CMD_LENGTH_CHG_STATUS 2
+
+// Linear data N values
+#define CMD_N_VALUE_VOUT_MODE -9
+#define CMD_N_VALUE_VOUT_COMMAND -9
+#define CMD_N_VALUE_VOUT_TRIM -9
+#define CMD_N_VALUE_IOUT_OC_FAULT_LIMIT -2
+#define CMD_N_VALUE_READ_VIN -1
+#define CMD_N_VALUE_READ_VOUT -9
+#define CMD_N_VALUE_READ_IOUT -2
+#define CMD_N_VALUE_READ_FAN_SPEED_1 5
+#define CMD_N_VALUE_READ_FAN_SPEED_2 5
+#define CMD_N_VALUE_CURVE_CC -2
+#define CMD_N_VALUE_CURVE_CV -9
+#define CMD_N_VALUE_CURVE_FV -9
+#define CMD_N_VALUE_CURVE_TC -2
+#define CMD_N_VALUE_CURVE_CC_TIMEOUT 0
+#define CMD_N_VALUE_CURVE_CV_TIMEOUT 0
+#define CMD_N_VALUE_CURVE_FLOAT_TIMEOUT 0
+
+// Curve Config bits
+// High Byte
+// 7-3 N/A
+#define FVTOE 2
+#define CVTOE 1
+#define CCTOE 0
+// Low Byte
+#define CUVE 7
+#define STGS 6
+// 5,4 N/A
+#define TCSHI 3
+#define TCSLO 2
+#define CUVSHI 1
+#define CUVSLO 0
+
+// Charge Status bits
+// High Byte
+#define FVTOF 7
+#define CVTOF 6
+#define CCTOF 5
+// 4 N/A
+#define BTNC 3
+#define NTCER 2
+// 1,0 N/A
+// Low Byte
+// 7-4 N/A
+#define FVM 3
+#define CVM 2
+#define CCM 1
+#define FULLM 0
+
+// System Config Bits
+// High Byte
+// 7-3 N/A
+#define EEP_OFF 2
+#define EEP_CONFIGHI 1
+#define EEP_CONFIGLO 0
+// Low Byte
+// 7-3 N/A
+#define OPERATION_INITHI 2
+#define OPERATION_INITLO 1
+#define PM_CTRL 0
+
+// System Status Bits
+// High Byte
+// 7-0 N/A
+// Low Byte
+// 7 N/A
+#define EEPER 6
+#define INITIAL_STATE 5
+#define ADL_ON 4
+// 3,2 N/A
+#define DC_OK 1
+// 0 N/A
---
+++ Meanwell-PMBus.cpp
@@ -0,0 +1,558 @@
+#include <Arduino.h>
+#include "rpb-1600.h"
+#include "rpb-1600-commands.h"
+#include "Wire.h"
+//#define RPB_1600_DEBUG
+
+#define NL Serial.print("\n");
+//----------------------------------------------------------------------
+// Public Functions
+//----------------------------------------------------------------------
+
+RPB_1600::RPB_1600()
+{
+}
+
+bool RPB_1600::Init(uint8_t chargerAddress)
+{
+    //int clockspeed = CLK * 1000;
+    my_charger_address = chargerAddress;
+    
+    Wire.setClock(100000);
+    
+    Wire.begin();
+#ifdef RPB_1600_DEBUG
+    //Serial.printf("SDA Pin: %d, SCL Pin: %d\n", SDA, SCL);
+    Serial.print(F("<RPB-1600 DEBUG> Init complete!\n"));
+#endif
+    return true;
+}
+
+bool RPB_1600::getReadings(readings *data)
+{
+    if (!readWithCommand(CMD_CODE_READ_VIN, CMD_LENGTH_READ_VIN))
+    {
+        return false;
+    }
+
+    data->v_in = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_READ_VOUT, CMD_LENGTH_READ_VOUT))
+    {
+        return false;
+    }
+
+    data->v_out = parseLinearVoltage(CMD_N_VALUE_READ_VOUT);
+
+    if (!readWithCommand(CMD_CODE_READ_IOUT, CMD_LENGTH_READ_IOUT))
+    {
+        return false;
+    }
+
+    data->i_out = parseLinearData();
+/*
+    if (!readWithCommand(CMD_CODE_READ_FAN_SPEED_1, CMD_LENGTH_READ_FAN_SPEED_1))
+    {
+        return false;
+    }
+
+    data->fan_speed_1 = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_READ_FAN_SPEED_2, CMD_LENGTH_READ_FAN_SPEED_2))
+    {
+        return false;
+    }
+
+    data->fan_speed_2 = parseLinearData();
+*/
+    return true;
+}
+#ifdef USECHARGER
+bool RPB_1600::getChargeStatus(charge_status *status)
+{
+    if (!readWithCommand(CMD_CODE_CHG_STATUS, CMD_LENGTH_CHG_STATUS))
+    {
+        return false;
+    }
+
+    parseChargeStatus(status);
+
+    return true;
+}
+
+bool RPB_1600::getCurveParams(curve_parameters *params)
+{
+    /* Query the charger for all charge curve related commands, and populate the following items:
+    params->cc;
+    params->cv;
+    params->floating_voltage;
+    params->taper_current;
+    params->config;
+    params->cc_timeout;
+    params->cv_timeout;
+    params->status; */
+
+    if (!readWithCommand(CMD_CODE_CURVE_CC, CMD_LENGTH_CURVE_CC))
+    {
+        return false;
+    }
+
+    params->cc = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_CURVE_CV, CMD_LENGTH_CURVE_CV))
+    {
+        return false;
+    }
+
+    params->cv = parseLinearVoltage(CMD_N_VALUE_CURVE_CV);
+
+    if (!readWithCommand(CMD_CODE_CURVE_FV, CMD_LENGTH_CURVE_FV))
+    {
+        return false;
+    }
+
+    params->floating_voltage = parseLinearVoltage(CMD_N_VALUE_CURVE_FV);
+
+    if (!readWithCommand(CMD_CODE_CURVE_TC, CMD_LENGTH_CURVE_TC))
+    {
+        return false;
+    }
+
+    params->taper_current = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_CURVE_CONFIG, CMD_LENGTH_CURVE_CONFIG))
+    {
+        return false;
+    }
+
+    parseCurveConfig(&params->config);
+
+    if (!readWithCommand(CMD_CODE_CURVE_CC_TIMEOUT, CMD_LENGTH_CURVE_CC_TIMEOUT))
+    {
+        return false;
+    }
+
+    params->cc_timeout = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_CURVE_CV_TIMEOUT, CMD_LENGTH_CURVE_CV_TIMEOUT))
+    {
+        return false;
+    }
+
+    params->cv_timeout = parseLinearData();
+
+    if (!readWithCommand(CMD_CODE_CURVE_FLOAT_TIMEOUT, CMD_LENGTH_CURVE_FLOAT_TIMEOUT))
+    {
+        return false;
+    }
+
+    params->float_timeout = parseLinearData();
+
+    getChargeStatus(&params->status);
+
+    return true;
+}
+#endif
+bool RPB_1600::readWithCommand(uint8_t commandID, uint8_t receiveLength)
+{
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Attempting to read command "));
+    Serial.print(commandID);
+    Serial.print(F(" with length "));
+    Serial.print(receiveLength);
+    Serial.print(F("\n"));
+#endif
+    // Send the command ID to the charger, and don't terminate the transmission
+    Wire.beginTransmission(my_charger_address);
+    Wire.write(commandID); // Write to I2C Tx buffer
+    Wire.endTransmission(false);
+
+    // Request bytes from the device
+    Wire.beginTransmission(my_charger_address);
+    Wire.requestFrom(my_charger_address, receiveLength);
+
+    clearRXBuffer();
+
+    // Count the number of bytes we receive
+    uint8_t num_bytes = 0;
+
+    // Pull bytes from the internal i2c RX buffer
+    while (Wire.available())
+    {
+        // Write the received byte to the buffer
+        my_rx_buffer[num_bytes] = Wire.read();
+
+#ifdef RPB_1600_DEBUG
+        Serial.print(F("<RPB-1600 DEBUG> Received byte "));
+        Serial.print(num_bytes);
+        Serial.print(F(" with value 0x"));
+        Serial.println(my_rx_buffer[num_bytes], HEX);
+        NL
+#endif
+
+        // Make sure we aren't getting more bytes then we expect
+        if (++num_bytes > MAX_RECEIVE_BYTES)
+        {
+            return false;
+        }
+    }
+
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Total bytes received: "));
+    Serial.println(num_bytes);
+    Serial.print("\n");
+
+    Serial.print(F("<RPB-1600 DEBUG> RX Buffer: ["));
+    for (int i = 0; i < num_bytes; i++)
+    {
+        Serial.print("0x");
+        if (my_rx_buffer[i] < 0x10)  // Add leading zero if needed
+        {
+            Serial.print("0");
+        }
+        Serial.print(my_rx_buffer[i], HEX);
+        if (i < num_bytes - 1)
+        {
+            Serial.print(",");
+        }
+    }
+    Serial.print("]\n");
+
+#endif
+
+    return num_bytes == receiveLength;
+}
+
+bool RPB_1600::writeTwoBytes(uint8_t commandID, uint8_t *data)
+{
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Attempting to write "));
+    Serial.print(data[0]);
+    Serial.print(F(" (low) and "));
+    Serial.print(data[1]);
+    Serial.print(F("(high) with command 0x"));
+    Serial.print(commandID);
+    NL
+#endif
+
+    Wire.beginTransmission(my_charger_address);
+    Wire.write(commandID);
+    uint8_t bytes_written = Wire.write(data, 2);
+    Wire.endTransmission();
+
+    return bytes_written == 2;
+}
+
+bool RPB_1600::writeLinearDataCommand(uint8_t commandID, int8_t N, int16_t value)
+{
+    uint16_t Y = 0;
+    uint16_t denominator = 0x0001;
+    // The Y value is calculated using the following equation
+    // (reference PMBUS spec rev 1.1 section 7.1): Value = Y * 2 ^ N
+    // Y = Value / (2 ^ N)
+    // We'll calculate denominator and do the math based on the polarity of N
+
+    if (N > 0)
+    {
+        denominator <<= N;
+        Y = value / denominator;
+    }
+    else if (N < 0)
+    {
+        denominator <<= (-N);
+        Y = value * denominator;
+    }
+
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Y calculation: Value = "));
+    Serial.print(value);
+    Serial.print(F(" Denom = "));
+    Serial.print(denominator);
+    Serial.print(F(" Y = "));
+    Serial.print(Y);
+    Serial.print("\n"); 
+#endif
+
+    return writeLinearDataHelper(commandID, N, Y);
+}
+
+bool RPB_1600::readManufData(mfr_data *mfrdata)
+{
+    // ID, Model, Revision, Location, Date, Serial
+    Serial.println(F("Reading Manu Data"));
+    int sizeofthing = CMD_LENGTH_MFR_ID;
+
+    if (!readWithCommand(CMD_CODE_MFR_ID, CMD_LENGTH_MFR_ID))
+    {
+        Serial.println(F("ERROR"));
+        return false;
+    }
+    //#ifdef RPB_1600_DEBUG
+        Serial.print(F("Sizeofthing: "));
+        Serial.println(sizeofthing);
+        Serial.println(F("ID Chars: "));
+    //#endif
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        //#ifdef RPB_1600_DEBUG
+        Serial.print(F("Num: "));
+        Serial.print(my_rx_buffer[i]);
+        Serial.print("  Char: ");
+            Serial.print((char)my_rx_buffer[i]);
+            Serial.print(F("\n"));
+        //#endif
+        mfrdata->id[i] = my_rx_buffer[i];
+    }
+
+    sizeofthing = CMD_LENGTH_MFR_MODEL;
+
+    if (!readWithCommand(CMD_CODE_MFR_MODEL, CMD_LENGTH_MFR_MODEL))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        mfrdata->model[i] = my_rx_buffer[i];
+    }
+
+    sizeofthing = CMD_LENGTH_MFR_REVISION;
+
+    if (!readWithCommand(CMD_CODE_MFR_REVISION, CMD_LENGTH_MFR_REVISION))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        mfrdata->revision[i] = my_rx_buffer[i];
+    }
+
+    sizeofthing = CMD_LENGTH_MFR_LOCATION;
+
+    if (!readWithCommand(CMD_CODE_MFR_LOCATION, CMD_LENGTH_MFR_LOCATION))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        mfrdata->location[i] = my_rx_buffer[i];
+    }
+
+    sizeofthing = CMD_LENGTH_MFR_DATE;
+
+    if (!readWithCommand(CMD_CODE_MFR_DATE, CMD_LENGTH_MFR_DATE))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        mfrdata->date[i] = my_rx_buffer[i];
+    }
+    
+    sizeofthing = CMD_LENGTH_MFR_SERIAL;
+
+    if (!readWithCommand(CMD_CODE_MFR_SERIAL, CMD_LENGTH_MFR_SERIAL))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < sizeofthing; ++i)
+    {
+        mfrdata->serial[i] = my_rx_buffer[i];
+    }
+}
+
+//----------------------------------------------------------------------
+// Private Functions
+//----------------------------------------------------------------------
+
+bool RPB_1600::writeLinearDataHelper(uint8_t commandID, int8_t N, int16_t Y)
+{
+    // Make sure the N value isn't bigger then 5 bits
+    if (abs(N) > 15)
+    {
+#ifdef RPB_1600_DEBUG
+        Serial.print(F("<RPB-1600 DEBUG> N value too large! Can't convert to linear format. N = "));
+        Serial.println(N);
+        NL
+#endif
+        return false;
+    }
+
+    // Make sure the Y (Mantissa) can fit in 11 bits
+    if (abs(Y) > 2047)
+    {
+#ifdef RPB_1600_DEBUG
+        Serial.print(F("<RPB-1600 DEBUG> Mantissa (Y) value too large! Can't convert to linear format. Y = "));
+        Serial.println(Y);
+#endif
+        return false;
+    }
+
+    // The goal here is to truncate Y to 11 bits, and put those in the lowest 11 bits of the outgoing data
+    // Then we need to truncate N to 5 bits, and put those bits in the highest 5 bits of the outgoing data
+    // See Section 7.1 of the PMBus 1.1 specification for more info on the "Linear Data Format"
+    // Note that data[0] is the low byte (sent first) and data[1] is the high byte (sent second)
+    uint8_t data[2] = {0x00, 0x00};
+
+    // Mask the lowest 8 bits of the mantissa, and put those bits in the low byte of the outgoing data
+    data[0] = Y & 0x00FF;
+    // Mask the lowest 3 bits of the high byte of the mantissa, and put those bits in the high byte of the outgoing data
+    data[1] = (Y & 0x0700) >> 8;
+    // Mask the lowest 5 bits of N, and shift them to be the highest 5 bytes of the high byte
+    data[1] = (N & 0x1F) << 3;
+#ifdef RPB_1600_DEBUG
+    Serial.print("<RPB-1600 DEBUG> Attempting to write linear data with N = ");
+    Serial.print(N);
+    Serial.print(" and Mantissa (Y) = ");
+    Serial.println(Y);
+#endif
+    writeTwoBytes(commandID, data);
+
+    return true;
+}
+
+uint16_t RPB_1600::parseLinearData(void)
+{
+    uint16_t rawData = my_rx_buffer[0] | (my_rx_buffer[1] << 8);
+
+    // Mask & shift out the "N" and "mantissa" values and convert them from 2s complement
+    int16_t rawN = (rawData & N_EXPONENT_MASK) >> N_EXPONENT_SHIFT;
+    int16_t N = UpscaleTwosComplement(rawN, N_EXPONENT_LENGTH);
+    int16_t rawMantissa = (rawData & MANTISSA_MASK);
+    int16_t mantissa = UpscaleTwosComplement(rawMantissa, MANTISSA_LENGTH);
+
+    uint16_t result = static_cast<uint16_t>(mantissa);
+    // The result is calculated by doing the following: result = mantissa * 2 ^ N
+    // however, N can be positive or negative, so we shift the mantissa accordingly
+    // to perform the multiplication by a power of 2.
+    if (N > 0)
+    {
+        result <<= N;
+    }
+    else if (N < 0)
+    {
+        result >>= (-N);
+    }
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Raw Data: 0x"));
+    Serial.println(rawData, HEX);
+
+    Serial.print(F("<RPB-1600 DEBUG> Raw N: 0x"));
+    Serial.println(rawN, HEX);
+
+    Serial.print(F("<RPB-1600 DEBUG> N: "));
+    Serial.println(N);
+
+    Serial.print(F("<RPB-1600 DEBUG> Raw Mantissa : 0x"));
+    Serial.println(rawMantissa, HEX);
+
+    Serial.print(F("<RPB-1600 DEBUG> Mantissa: "));
+    Serial.println(mantissa);
+
+    Serial.print(F("<RPB-1600 DEBUG> Result: "));
+    Serial.println(result);
+
+#endif
+
+    return result;
+}
+
+float RPB_1600::parseLinearVoltage(int8_t N)
+{
+    uint16_t rawData = my_rx_buffer[0] | (my_rx_buffer[1] << 8);
+
+    // Calculate divisor using N
+    uint16_t divisor = 0x01 << abs(N);
+
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Parsing linear voltage w/N: "));
+    Serial.println(N);
+    NL
+#endif
+
+    float result = (float)rawData;
+
+    // Divide based on the polarity of N
+    if (N > 0)
+    {
+        result /= -divisor;
+    }
+    else if (N < 0)
+    {
+        result /= divisor;
+    }
+
+#ifdef RPB_1600_DEBUG
+    Serial.print(F("<RPB-1600 DEBUG> Linear voltage result: "));
+    Serial.println(result);
+    NL
+#endif
+
+    return result;
+}
+#ifdef USECHARGER
+void RPB_1600::parseCurveConfig(curve_config *config)
+{
+    // Bits 0 & 1 of low byte
+    config->charge_curve_type = (my_rx_buffer[0] & 0x02);
+    // Bits 2 & 3 of low byte
+    config->temp_compensation = (my_rx_buffer[0] & 0x0C) >> 2;
+    // Bit 7 of low byte (0 = 3 stage, 1 = 2 stage)
+    config->num_charge_stages = (my_rx_buffer[0] && 0x40) ? 2 : 3;
+    // Bit 0 of high byte
+    config->cc_timeout_indication_enabled = (my_rx_buffer[1] && 0x01);
+    // Bit 1 of high byte
+    config->cv_timeout_indication_enabled = (my_rx_buffer[1] && 0x02);
+    // Bit 2 of high byte
+    config->float_stage_timeout_indication_enabled = (my_rx_buffer[1] && 0x04);
+} 
+
+void RPB_1600::parseChargeStatus(charge_status *status)
+{
+    // Low byte:
+    status->fully_charged = (my_rx_buffer[0] && 0x01); // Bit 0
+    status->in_cc_mode = (my_rx_buffer[0] && 0x02);    // Bit 1
+    status->in_cv_mode = (my_rx_buffer[0] && 0x04);    // Bit 2
+    status->in_float_mode = (my_rx_buffer[0] && 0x08); // Bit 3
+    // High byte:
+    status->EEPROM_error = (my_rx_buffer[1] && 0x01);                    // Bit 0
+    status->temp_compensation_short_circuit = (my_rx_buffer[1] && 0x04); // Bit 1
+    status->battery_detected = (my_rx_buffer[1] && 0x08);                // Bit 3
+    status->timeout_flag_cc_mode = (my_rx_buffer[1] && 0x20);            // Bit 5
+    status->timeout_flag_cv_mode = (my_rx_buffer[1] && 0x40);            // Bit 6
+    status->timeout_flag_float_mode = (my_rx_buffer[1] && 0x80);         // Bit 7
+}
+#endif
+// Slightly modified version of this https://www.codeproject.com/Tips/1079637/Twos-Complement-for-Unusual-Integer-Sizes
+int16_t RPB_1600::UpscaleTwosComplement(int16_t value, size_t length)
+{
+    uint16_t mask = (~0) << length;
+    // Too small for complement?
+    if (length < 2)
+    {
+        return (~mask & value);
+    }
+    // Two's complement?
+    uint16_t msb = 1 << (length - 1);
+    if (value & msb)
+    {
+        return (mask | value);
+    }
+    else
+    {
+        return (~mask & value);
+    }
+}
+
+void RPB_1600::clearRXBuffer(void)
+{
+    memset(my_rx_buffer, 0, MAX_RECEIVE_BYTES);
+#ifdef RPB_1600_DEBUG
+    Serial.print("<RPB-1600 DEBUG> RX Buffer Cleared!\n");
+#endif
+}
